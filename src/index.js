e016f21cbe30a2afac4098b8a3ef24ac2966d820
Filip55561 <88946851+Filip55561@users.noreply.github.com>
2024-09-06 01:06:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;  // Use the default port provided by Koyeb
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => res.send('Bot is running!'));
 
