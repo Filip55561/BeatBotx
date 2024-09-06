305983020c3e0e6cdc5c268f77e335bd7ef66d00
Filip55561 <88946851+Filip55561@users.noreply.github.com>
2024-09-06 19:35:45 +0200

    I FOUND THE PROBLEM

--- src/index.js
+++ src/index.js
@@ -24,10 +24,10 @@ const client = new Client({
 
 client.commands = new Collection();
 const fs = require('fs');
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`/src/commands/${file}`);
+    const command = require(`src/commands/${file}`);
     client.commands.set(command.data.name, command);
 }
 
