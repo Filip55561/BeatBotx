5867d7f22dbe7327bdb3333e6966642ceace53f1
Filip55561 <88946851+Filip55561@users.noreply.github.com>
2024-09-22 14:41:46 +0200

    Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -1,20 +1,36 @@
-const { Guild } = require('discord.js');
+const { PermissionFlagsBits } = require('discord.js');
 
 async function logCommand(guild, userTag, commandName, args = '', channel, user) {
     if (commandName === "membercount" || commandName === "flip") return;
-    
-    const logChannel = guild.channels.cache.find(ch => ch.name === 'logs' && ch.type === 'GUILD_TEXT');
 
-    if (!logChannel) {
-        console.error('Log channel not found!');
-        return;
-    }
+    let logChannel;
 
-    // Log the command execution
-    if (user !== null) {
-        await logChannel.send(`Command executed: /${commandName} ${args} by ${userTag} in ${channel} for ${user}`);
-    } else {
-        await logChannel.send(`Command executed: /${commandName} ${args} by ${userTag} in ${channel}`);
+    try {
+        logChannel = guild.channels.cache.find(ch => ch.name === 'logs' && ch.type === 0);
+
+        if (!logChannel) {
+            // Create a new "logs" channel if not found
+            logChannel = await guild.channels.create({
+                name: 'logs',
+                type: 0, 
+                permissionOverwrites: [
+                    {
+                        id: guild.roles.everyone.id,
+                        deny: [PermissionFlagsBits.ViewChannel],
+                    },
+                ],
+            });
+            console.log('Created new "logs" channel.');
+        }
+
+        // Log the command execution in the found or created log channel
+        if (user !== null) {
+            await logChannel.send(`Command executed: /${commandName} ${args} by ${userTag} in ${channel} for ${user}`);
+        } else {
+            await logChannel.send(`Command executed: /${commandName} ${args} by ${userTag} in ${channel}`);
+        }
+    } catch (error) {
+        console.error('Error fetching or creating log channel:', error);
     }
 }
 
